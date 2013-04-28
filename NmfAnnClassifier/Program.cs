8906812fb813870baa3e3a3dd094d903dd8598d1
Travis Silvers <firestrand@gmail.com>
2013-04-27 22:50:55 -0400

    Initial Factoring

--- NmfAnnClassifier/Program.cs
+++ NmfAnnClassifier/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using DotNetMatrix;
@@ -15,21 +17,38 @@ namespace NmfAnnClassifier
         {
             //Execute and create the non-negative matrix factorized values from the unlabeled source data
             //TODO: Replace testing matrix
-            const int packedWRows = 3;
-            const int packedHRows = 2;
-            var packedKnownW = new[] { 1.0, 3.0, 2.0, 5.0, 1.0, 7.0 };
-            var packedKnownH = new[] { 3.0, 8.0, 1.0, 3.0, 4.0, 7.0 };
-            var w = new GeneralMatrix(packedKnownW, packedWRows);
-            var h = new GeneralMatrix(packedKnownH, packedHRows);
-            var v = w * h;
+            const int packedWRows = 1875;
+            const int packedHRows = 81;
+            //var packedKnownW = new[] { 1.0, 3.0, 2.0, 5.0, 1.0, 7.0 };
+            //var packedKnownH = new[] { 3.0, 8.0, 1.0, 3.0, 4.0, 7.0 };
+            //var w = new GeneralMatrix(packedKnownW, packedWRows);
+            //var h = new GeneralMatrix(packedKnownH, packedHRows);
+            //var v = w * h;
 
+            var list = new List<double>(1875*10000);
+            var rows = 0;
             //Get the matrix / array to factor
-            
+            using(var reader = File.OpenText("test.csv"))
+            {
+                string s;
+                //Skip first line
+                reader.ReadLine();
+                while((s = reader.ReadLine()) != null)
+                {
+                    //Read into memory
+                    list.AddRange(s.Split(new[] {','}).Select(Double.Parse));
+                    rows++;
+                }
+            }
+            //var matrix = new GeneralMatrix(list.ToArray(), rows);
+            var v = list.ToArray();
+            //Scale each value from -2,2 to 0, 4
+
             //Create the problem from the matrix
-            var problem = new NonNegativeMatrixFactorization(packedWRows, packedHRows, v.ColumnPackedCopy,false);
+            var problem = new MatrixFactorization(packedWRows, packedHRows, v ,-2,2);
             
             //Specify stoping conditions
-            int numIterations = 1000000;
+            int numIterations = 100000;
             IRunCondition runCondition = new RunConditionFitness(numIterations, problem.AcceptableFitness);
             problem.RunCondition = runCondition;
             
@@ -46,10 +65,9 @@ namespace NmfAnnClassifier
 
             Console.WriteLine("Fitness: {0}",result.Fitness);
             Console.Write("Result: ");
-            foreach (var val in best)
-            {
-                Console.Write("{0} ", val);
-            }
+            File.WriteAllText("result.csv", String.Join(",",best.Select(dv=>dv.ToString(CultureInfo.InvariantCulture))));
+                
+            Console.WriteLine("ALL DONE!");
             Console.ReadKey();
         }
     }
